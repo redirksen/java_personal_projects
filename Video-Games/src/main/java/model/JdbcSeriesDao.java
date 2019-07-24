c7b0713e8cd63e5a5bce1a3613eeea6b7cc9a236
redirksen <redirksen@gmail.com>
2019-07-24 08:49:15 -0400

    Series DAO code

--- Video-Games/src/main/java/model/JdbcSeriesDao.java
+++ Video-Games/src/main/java/model/JdbcSeriesDao.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,12 +19,34 @@ public class JdbcSeriesDao {
 
 	// get info from database
 	public Series getSeries(int id) {
-		return null;
+		Series selectedSeries = new Series();
+		String sqlSeries = "SELECT * FROM series WHERE series_id = ?";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlSeries, id);
+		while (results.next()) {
+			selectedSeries.setSeriesId(results.getInt("series_id"));
+			selectedSeries.setSeriesName(results.getString("series_name"));
+		}
+		return selectedSeries;
 	}
 
 	// add series
 	public void createSeries(Series series) {
+		int id = getNextId();
+		String sqlInsertResult = "INSERT INTO series(series_id, series_name) VALUES (?,?)";
+		jdbcTemplate.update(sqlInsertResult, id, series.getSeriesName());
+		series.setSeriesId(id);
+	}
 
+	private int getNextId() {
+		String sqlSelectNextId = "SELECT NEXTVAL('series_series_id_seq')";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlSelectNextId);
+		Integer id = null;
+		if (results.next()) {
+			id = results.getInt(1);
+		} else {
+			throw new RuntimeException("Something strange happened, unable to select next forum post id from sequence");
+		}
+		return id;
 	}
 
 }
